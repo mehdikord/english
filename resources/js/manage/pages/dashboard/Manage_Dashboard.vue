395654a9de4cad8ada6df24331fb46311e928a3e
mehdikord <mehdi.kord171@gmail.com>
2023-05-29 23:12:53 +0330

    add episode level

--- resources/js/manage/pages/dashboard/Manage_Dashboard.vue
+++ resources/js/manage/pages/dashboard/Manage_Dashboard.vue
@@ -1,10 +1,25 @@
 <template>
-this is dashboard
+    <div class="row">
+        <div class="col-12">
+            <Dashboard_Count></Dashboard_Count>
+
+        </div>
+
+
+    </div>
+
+
+
 </template>
 
 <script>
+import Manage_Dashboard_Inc_Count from "./includes/Manage_Dashboard_Inc_Count.vue";
+
 export default {
-    name: "Manage_Dashboard"
+    name: "Manage_Dashboard",
+    components : {
+        "Dashboard_Count" : Manage_Dashboard_Inc_Count,
+    }
 }
 </script>
 
---
+++ resources/js/manage/pages/dashboard/includes/Manage_Dashboard_Inc_Count.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="row">
+        <div class="col-md-4">
+            <q-card class="bg-blue-8 text-white">
+                <q-card-section>
+                    <strong class="float-right font-32">9</strong>
+
+                    <div class="text-h6">System Administrator</div>
+
+                    <div class="text-subtitle2 q-mt-sm">
+                        system managers count
+                    </div>
+                </q-card-section>
+
+                <q-card-actions>
+                    <q-btn glossy color="teal" class="w-100" >All managers</q-btn>
+
+                </q-card-actions>
+            </q-card>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Manage_Dashboard_Inc_Count"
+}
+</script>
+
+<style scoped>
+
+</style>
